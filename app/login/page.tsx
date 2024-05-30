607feff566774afb309505fe7f21496ebf8f5585
bora001 <bora.vancouver@gmail.com>
2024-05-30 21:28:57 +0900

    add ui package

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,14 @@
+import { Button } from '@/components/ui/button';
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
+
 const page = () => {
-  return (
-    <div>Login</div>
-  )
-}
-export default page
+ return (
+  <div className="item-center  flex h-screen w-screen items-center justify-center">
+   <Button className="space-x-4 bg-black hover:bg-blue-900">
+    <GitHubLogoIcon />
+    <p>Github Login</p>
+   </Button>
+  </div>
+ );
+};
+export default page;
